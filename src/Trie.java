920ea9781321442cdf0ab86e7b4b1aae67c6169c
Peter Antonaros <46857643+peterantonarosjr@users.noreply.github.com>
2021-07-12 17:47:26 -0400

    Function updates

--- src/Trie.java
+++ src/Trie.java
@@ -5,15 +5,36 @@ public class Trie {
         root = new TrieNode();
     }
 
+    //Insert a word into the tree if it's new
+    public void insertWord(String word){
+        if(!isWordIn(word)){
+            //Insert the word into Trie Code
+        }
 
+    }
 
-    //Insert word function
+    //Does the word exist already in Trie?
+    public boolean isWordIn(String word){
+        if(searchTrie(word)) {
+            return true;
+        }
+        return false;
+    }
 
-    //Search for word
+    //Does the prefix exist already in Trie?
+    public boolean isPrefixIn(String prefix){
+        if(searchTrie(prefix)){
+            return true;
+        }
+        return false;
+    }
 
-    //Search for prefix
+    //Search for given value
+    private boolean searchTrie(String value){
 
-    //Display trie
+        return true;
+    }
+
 
 
 }
